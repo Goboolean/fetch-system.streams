9438faf6af5644fe51840aae7827eb7149a08c79
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-12-29 21:03:13 +0900

    create JsonSerde for pure json object

--- src/main/java/io/goboolean/streams/serde/JsonSerde.java
+++ src/main/java/io/goboolean/streams/serde/JsonSerde.java
@@ -1,86 +1,53 @@
 package io.goboolean.streams.serde;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.protobuf.Descriptors.FieldDescriptor;
-import com.google.protobuf.Message;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serializer;
+
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
-public class JsonSerde<T extends com.google.protobuf.GeneratedMessageV3> implements Serde<T> {
-    private final Serializer<T> serializer;
-    private final Deserializer<T> deserializer;
+public class JsonSerde<T> implements Serde<T> {
 
-    public JsonSerde(Serializer<T> serializer, Deserializer<T> deserializer) {
-        this.serializer = serializer;
-        this.deserializer = deserializer;
+    private final Gson gson = new Gson();
+    private final Class<T> targetType;
+
+    public JsonSerde(Class<T> targetType) {
+        this.targetType = targetType;
     }
 
     @Override
     public Serializer<T> serializer() {
-        return serializer;
+        return new JsonSerializer();
     }
 
     @Override
     public Deserializer<T> deserializer() {
-        return deserializer;
+        return new JsonDeserializer();
     }
 
-    public static class JsonSerializer<T extends com.google.protobuf.GeneratedMessageV3> implements Serializer<T> {
-        private final Gson gson = new Gson();
-
-        @Override
-        public void configure(Map<String, ?> configs, boolean isKey) {}
-
+    private class JsonSerializer implements Serializer<T> {
         @Override
         public byte[] serialize(String topic, T data) {
-
-            JsonObject jsonObject = new JsonObject();
-
-            for (FieldDescriptor field : data.getDescriptorForType().getFields()) {
-                Object value = data.getField(field);
-
-                switch (field.getType()) {
-                    case INT32:
-                    case INT64:
-                    case FLOAT:
-                    case DOUBLE:
-                    case BOOL:
-                        jsonObject.addProperty(field.getName(), (Number) value);
-                        break;
-                    case STRING:
-                        jsonObject.addProperty(field.getName(), (String) value);
-                        break;
-                    default:
-                        throw new RuntimeException("Unsupported type: " + field.getType());
-                }
-            }
-
-            String jsonString = gson.toJson(jsonObject);
-            return jsonString.getBytes();
+            return gson.toJson(data).getBytes(StandardCharsets.UTF_8);
         }
-
-        @Override
-        public void close() {}
     }
 
-    public static class JsonDeserializer<T extends com.google.protobuf.GeneratedMessageV3> implements Deserializer<T> {
-        private final Gson gson = new Gson();
-
-        public JsonDeserializer() {}
-
-        @Override
-        public void configure(Map<String, ?> configs, boolean isKey) {}
-
+    private class JsonDeserializer implements Deserializer<T> {
         @Override
         public T deserialize(String topic, byte[] data) {
-            throw new RuntimeException("Not implemented");
+            if (data == null) {
+                return null;
+            }
+            return gson.fromJson(new String(data, StandardCharsets.UTF_8), targetType);
         }
-
-        @Override
-        public void close() {}
     }
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {}
+
+    @Override
+    public void close() {}
 }
