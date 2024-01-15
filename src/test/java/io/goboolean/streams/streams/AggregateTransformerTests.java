798a4ac62e5277d530f7ee904f3a6f2128377173
mulmuri <68294539+mulmuri@users.noreply.github.com>
2024-01-15 18:58:52 +0900

    fix aggregate transformer for unexpected case

--- src/test/java/io/goboolean/streams/streams/AggregateTransformerTests.java
+++ src/test/java/io/goboolean/streams/streams/AggregateTransformerTests.java
@@ -3,11 +3,12 @@ package io.goboolean.streams.streams;
 
 import io.goboolean.streams.serde.Model;
 import org.apache.kafka.streams.KeyValue;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.Duration;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,12 +16,11 @@ import java.util.List;
 public class AggregateTransformerTests {
 
     private AggregateTransformer aggregateTransformer;
-
-    private ArrayList<Model.Aggregate> store = new ArrayList<>();
+    private final String symbol = "stock.goboolean.io";
 
     private List<Model.Aggregate> aggregates = Arrays.asList(
             new Model.Aggregate(
-                    "TRADE",
+                    symbol,
                     20,
                     10,
                     30,
@@ -30,7 +30,7 @@ public class AggregateTransformerTests {
                     ZonedDateTime.parse("2024-01-04T15:30:45Z")
             ),
             new Model.Aggregate(
-                    "TRADE",
+                    symbol,
                     30,
                     30,
                     40,
@@ -40,7 +40,7 @@ public class AggregateTransformerTests {
                     ZonedDateTime.parse("2024-01-04T15:30:47Z")
             ),
             new Model.Aggregate(
-                    "TRADE",
+                    symbol,
                     40,
                     30,
                     50,
@@ -48,11 +48,21 @@ public class AggregateTransformerTests {
                     35,
                     2,
                     ZonedDateTime.parse("2024-01-04T15:30:51Z")
+            ),
+            new Model.Aggregate(
+                    symbol,
+                    50,
+                    20,
+                    60,
+                    20,
+                    55,
+                    2,
+                    ZonedDateTime.parse("2024-01-04T15:30:54Z")
             )
     );
     private List<Model.Aggregate> result = Arrays.asList(
             new Model.Aggregate(
-                    "TRADE",
+                    symbol,
                     20,
                     30,
                     40,
@@ -60,31 +70,40 @@ public class AggregateTransformerTests {
                     25,
                     6,
                     ZonedDateTime.parse("2024-01-04T15:30:45Z")
+            ),
+            new Model.Aggregate(
+                    symbol,
+                    40,
+                    20,
+                    60,
+                    20,
+                    45,
+                    4,
+                    ZonedDateTime.parse("2024-01-04T15:30:50Z")
             )
     );
 
     public AggregateTransformerTests() {
         this.aggregateTransformer = new AggregateTransformer(
-                store,
-                new TimeTruncationer.FiveSecTruncationer());
+                "",
+                new TimeTruncationer.FiveSecTruncationer(), Duration.ofSeconds(1));
     }
 
     @Test
     public void testTransformScenario() {
 
         KeyValue<Integer, Model.Aggregate> kv0 = aggregateTransformer.transform(0, aggregates.get(0));
-        assert kv0 == null;
-        assert store.size() == 1;
-        assert store.get(0).equals(aggregates.get(0));
+        Assertions.assertNull(kv0);
 
         KeyValue<Integer, Model.Aggregate> kv1 = aggregateTransformer.transform(0, aggregates.get(1));
-        assert kv1 == null;
-        assert store.size() == 2;
-        assert store.get(1).equals(aggregates.get(1));
+        Assertions.assertNull(kv1);
 
         KeyValue<Integer, Model.Aggregate> kv2 = aggregateTransformer.transform(0, aggregates.get(2));
-        assert kv2.value.equals(result.get(0));
-        assert store.size() == 1;
-        assert store.get(0).equals(aggregates.get(2));
+        Assertions.assertNotNull(kv2);
+        Assertions.assertEquals(kv2.value, result.get(0));
+
+        KeyValue<Integer, Model.Aggregate> kv3 = aggregateTransformer.transform(0, aggregates.get(3));
+        Assertions.assertNotNull(kv3);
+        Assertions.assertEquals(kv3.value, result.get(1));
     }
 }
