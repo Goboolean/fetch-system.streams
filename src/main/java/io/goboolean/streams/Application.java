7c1ff49bbf1fd08a84bb9da7dee9aa3794bf5086
mulmuri <68294539+mulmuri@users.noreply.github.com>
2024-01-17 15:31:48 +0900

    create metrics

--- src/main/java/io/goboolean/streams/Application.java
+++ src/main/java/io/goboolean/streams/Application.java
@@ -1,67 +1,17 @@
 package io.goboolean.streams;
 
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
-import io.opentelemetry.context.propagation.ContextPropagators;
-import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
-import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
-import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.logs.SdkLoggerProvider;
-import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
-import io.opentelemetry.sdk.metrics.SdkMeterProvider;
-import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
-import io.opentelemetry.sdk.resources.Resource;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
-import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
 public class Application {
 
-	private String serviceName = "streams";
-	private String serviceVersion = "0.1.0";
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(Application.class);
 		app.setBannerMode(Banner.Mode.OFF);
 		app.run(args);
 	}
-
-	@Bean
-	public OpenTelemetry openTelemetry() {
-
-		Resource resource = Resource.getDefault().toBuilder().put(ResourceAttributes.SERVICE_NAME, serviceName).put(ResourceAttributes.SERVICE_VERSION, serviceVersion).build();
-
-		SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
-				.addSpanProcessor(BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder().build()).build())
-				.setResource(resource)
-				.build();
-
-		SdkMeterProvider sdkMeterProvider = SdkMeterProvider.builder()
-				.registerMetricReader(PeriodicMetricReader.builder(OtlpGrpcMetricExporter.builder().build()).build())
-				.setResource(resource)
-				.build();
-
-		SdkLoggerProvider sdkLoggerProvider = SdkLoggerProvider.builder()
-				.addLogRecordProcessor(
-						BatchLogRecordProcessor.builder(OtlpGrpcLogRecordExporter.builder().build()).build())
-				.setResource(resource)
-				.build();
-
-		OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
-				.setTracerProvider(sdkTracerProvider)
-				.setMeterProvider(sdkMeterProvider)
-				.setLoggerProvider(sdkLoggerProvider)
-				.setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-				.buildAndRegisterGlobal();
-
-		return openTelemetry;
-	}
-
 }
---
+++ src/main/java/io/goboolean/streams/config/OpenTelemetryConfig.java
@@ -0,0 +1,145 @@
+package io.goboolean.streams.config;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
+import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
+import io.opentelemetry.sdk.logs.export.LogRecordExporter;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.export.MetricExporter;
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class OpenTelemetryConfig {
+
+    @Value("${open-telemetry.endpoint}")
+    private String endPoint;
+
+    @Value("${open-telemetry.service-name}")
+    private String serviceName;
+
+    @Value("${open-telemetry.service-version}")
+    private String serviceVersion;
+
+    @Bean
+    public OpenTelemetry openTelemetry() {
+
+        Resource resource = Resource.getDefault().toBuilder()
+                .put(ResourceAttributes.SERVICE_NAME, serviceName)
+                .put(ResourceAttributes.SERVICE_VERSION, serviceVersion)
+                .build();
+
+        MetricExporter metricExporter = OtlpGrpcMetricExporter.builder()
+                .setEndpoint(endPoint)
+                .setTimeout(30, TimeUnit.SECONDS)
+                .build();
+
+        LogRecordExporter logRecordExporter = OtlpGrpcLogRecordExporter.builder()
+                .setEndpoint(endPoint)
+                .setTimeout(30, TimeUnit.SECONDS)
+                .build();
+
+        SdkMeterProvider sdkMeterProvider = SdkMeterProvider.builder()
+                .registerMetricReader(
+                        PeriodicMetricReader.builder(metricExporter)
+                                .build())
+                .setResource(resource)
+                .build();
+
+        SdkLoggerProvider sdkLoggerProvider = SdkLoggerProvider.builder()
+                .addLogRecordProcessor(
+                        BatchLogRecordProcessor.builder(logRecordExporter)
+                                .build())
+                .setResource(resource)
+                .build();
+
+        OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
+                .setMeterProvider(sdkMeterProvider)
+                .setLoggerProvider(sdkLoggerProvider)
+                .buildAndRegisterGlobal();
+
+        return openTelemetry;
+    }
+
+    @Bean(name = "fetch-system.streams.received.trade")
+    public LongCounter receivedTradeCounter(OpenTelemetry openTelemetry) {
+        return openTelemetry
+                .getMeter("fetch-system.streams")
+                .counterBuilder("fetch-system.streams.received.trade")
+                .setDescription("")
+                .build();
+    }
+
+    @Bean(name = "fetch-system.streams.exported.trade")
+    public LongCounter exportedTradeCounter(OpenTelemetry openTelemetry) {
+        return openTelemetry
+                .getMeter("fetch-system.streams")
+                .counterBuilder("fetch-system.streams.received.trade")
+                .setDescription("")
+                .build();
+    }
+
+    @Bean(name = "fetch-system.streams.received.1s")
+    public LongCounter received1sAggregateCounter(OpenTelemetry openTelemetry) {
+        return openTelemetry
+                .getMeter("fetch-system.streams")
+                .counterBuilder("fetch-system.streams.received.1s-aggregate")
+                .setDescription("")
+                .build();
+    }
+
+    @Bean(name = "fetch-system.streams.exported.1s")
+    public LongCounter exported1sAggregateCounter(OpenTelemetry openTelemetry) {
+        return openTelemetry
+                .getMeter("fetch-system.streams")
+                .counterBuilder("fetch-system.streams.exported.1s-aggregate")
+                .setDescription("")
+                .build();
+    }
+
+    @Bean(name = "fetch-system.streams.received.5s")
+    public LongCounter received5sAggregateCounter(OpenTelemetry openTelemetry) {
+        return openTelemetry
+                .getMeter("fetch-system.streams")
+                .counterBuilder("fetch-system.streams.received.5s-aggregate")
+                .setDescription("")
+                .build();
+    }
+
+    @Bean(name = "fetch-system.streams.exported.5s")
+    public LongCounter received1mAggregateCounter(OpenTelemetry openTelemetry) {
+        return openTelemetry
+                .getMeter("fetch-system.streams")
+                .counterBuilder("fetch-system.streams.received.1m-aggregate")
+                .setDescription("")
+                .build();
+    }
+
+    @Bean(name = "fetch-system.streams.received.1m")
+    public LongCounter exported1mAggregateCounter(OpenTelemetry openTelemetry) {
+        return openTelemetry
+                .getMeter("fetch-system.streams")
+                .counterBuilder("fetch-system.streams.exported.1m-aggregate")
+                .setDescription("")
+                .build();
+    }
+
+    @Bean(name = "fetch-system.streams.received.5m")
+    public LongCounter received5mAggregateCounter(OpenTelemetry openTelemetry) {
+        return openTelemetry
+                .getMeter("fetch-system.streams")
+                .counterBuilder("fetch-system.streams.received.5m-aggregate")
+                .setDescription("")
+                .build();
+    }
+}
